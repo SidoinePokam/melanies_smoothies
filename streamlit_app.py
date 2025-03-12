a893da3ae0b6d4f02c19a05494242bbb9b679565
SidoinePokam <75645705+SidoinePokam@users.noreply.github.com>
2025-03-12 03:17:52 +0100

    Update streamlit_app.py
    add api to display smoothiefroot nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,9 @@ st.write(
     """
 )
 
+name_on_order = st.text_input("Name on Smoothie : ")
+st.write("The name on your Smoothie will be : ", name_on_order)
+
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
@@ -28,8 +31,8 @@ if ingredients_list :
 
     #st.write(ingredients_string)
 
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+            values ('""" + ingredients_string + """',""" + name_on_order + """')"""
 
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')
@@ -38,3 +41,8 @@ if ingredients_list :
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered!', icon="✅")
+
+# New section to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon"")
+st.text(smoothiefroot_response)
